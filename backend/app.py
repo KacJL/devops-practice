1198478345741a7ca5081e8f9d923f79fa50b3f7
Vadim Pushka <49stay.wild49@gmail.com>
2026-08-13 18:23:15 +0300

    first commit

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+
+# Используем переменные окружения для гибкости (DevOps style)
+REDIS_HOST = os.getenv("REDIS_HOST", "db") 
+redis = Redis(host=REDIS_HOST, port=6379, decode_responses=True)
+
+@app.route('/api')
+def hello():
+    try:
+        count = redis.incr('hits')
+        return f'Привет! Этот сайт открывали {count} раз.'
+    except Exception as e:
+        return f"Ошибка БД: {str(e)}", 500
+
+if __name__ == "__main__":
+    # debug=True полезен при разработке
+    app.run(host="0.0.0.0", port=5000, debug=False)
